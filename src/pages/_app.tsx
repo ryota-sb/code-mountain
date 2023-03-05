c758f1600d97d3406ab6cdb3850808bd583bf5ba
ryota-sb <ryota.sb.1206@gmail.com>
2023-03-05 21:40:00 +0900

    next/fontでデフォルトのフォント設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "styles/globals.css";
+import type { AppProps } from "next/app";
+
+// フォント適用
+import { Gloock } from "next/font/google";
+const gloock = Gloock({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={gloock.className}>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
