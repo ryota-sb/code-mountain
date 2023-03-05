e2295f1c4258033fb078af66687245863c70ce91
ryota-sb <ryota.sb.1206@gmail.com>
2023-03-05 22:34:56 +0900

    必要なページ作成

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import Layout from "components/Layout";
+
+const About: NextPage = () => {
+  return (
+    <Layout>
+      <div className="min-h-screen flex-grow">
+        <h1>About Page.</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ src/pages/product.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import Layout from "components/Layout";
+
+const Product: NextPage = () => {
+  return (
+    <Layout>
+      <div className="min-h-screen flex-grow">
+        <h1>Product Page.</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Product;
