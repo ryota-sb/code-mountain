ba87a58a5f155d90d7e70111525296e35420eb09
ryota-sb <ryota.sb.1206@gmail.com>
2023-03-06 00:09:20 +0900

    フォント修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,13 @@ import "styles/globals.css";
 import type { AppProps } from "next/app";
 
 // フォント適用
-import { Gloock } from "next/font/google";
-const gloock = Gloock({ subsets: ["latin"], weight: "400" });
+import { Raleway } from "next/font/google";
+const raleway = Raleway({ subsets: ["latin"], weight: "200" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={gloock.className}>
-      <Component {...pageProps} />;
+    <div className={raleway.className}>
+      <Component {...pageProps} />
     </div>
   );
 }
