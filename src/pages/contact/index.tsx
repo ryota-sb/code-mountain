3b8ee388b503079e94fc3b88be0115fc0073ed07
ryota-sb <ryota.sb.1206@gmail.com>
2023-03-09 19:59:04 +0900

    お問合せページレイアウト完了

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,10 +1,30 @@
 import { NextPage } from "next";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import Layout from "components/Layout";
+
+import { ContactFormInput } from "types";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 import { Montserrat } from "next/font/google";
 const roboto = Montserrat({ subsets: ["latin"], weight: "700" });
 
 const Contact: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ContactFormInput>({
+    mode: "onChange",
+    defaultValues: { name: "", email: "", message: "" },
+  });
+
+  const onSubmit: SubmitHandler<ContactFormInput> = () => {
+    console.log("フォーム送信ボタンを押しました。");
+  };
   return (
     <Layout>
       <div className="mx-10 mt-20 flex min-h-screen flex-col gap-20">
@@ -13,6 +33,55 @@ const Contact: NextPage = () => {
         >
           CANTACT
         </h1>
+        <div className="h-full rounded-3xl p-10">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-10">
+              <div className="grid grid-cols-2 gap-20">
+                <div className="flex flex-col gap-y-3">
+                  <label>お名前</label>
+                  <input
+                    {...register("name", { required: true })}
+                    className="rounded-3xl border border-white bg-gray-500 bg-opacity-0 py-2 px-4"
+                  />
+                  {errors.name && (
+                    <h1 className="text-red-500">お名前を入力してください</h1>
+                  )}
+                </div>
+                <div className="flex flex-col gap-y-3">
+                  <label>メールアドレス</label>
+                  <input
+                    {...register("email", { required: true })}
+                    className="rounded-3xl border border-white bg-gray-500 bg-opacity-0 py-2 px-4"
+                  />
+                  {errors.email && (
+                    <h1 className="text-red-500">
+                      メールアドレスを入力してください
+                    </h1>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col gap-y-3">
+                <label>メッセージ内容</label>
+                <textarea
+                  {...register("message", { required: true })}
+                  className="h-96 rounded-3xl border border-white bg-gray-500 bg-opacity-0 py-2 px-4"
+                  placeholder="メッセージを記入してください。"
+                />
+                {errors.message && (
+                  <h1 className="text-red-500">メッセージを入力してください</h1>
+                )}
+              </div>
+            </div>
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="my-4 h-14 w-14 cursor-pointer rounded-full bg-gray-500 bg-opacity-10 hover:scale-110 hover:duration-500"
+              >
+                <FontAwesomeIcon icon={faPaperPlane} />
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </Layout>
   );
