ff70f802448e28f6451586a25699a162e1e77305
ryota-sb <ryota.sb.1206@gmail.com>
2023-04-23 23:48:43 +0900

    ホバー時に、背景にブラーを描けるように変更

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -12,7 +12,11 @@ const Product: NextPage = () => {
   const [isLoading, setLoading] = useState<boolean>(true);
 
   const products = [
-    { name: "Code Atelier", img: "/images/woman.webp" },
+    {
+      name: "Code Atelier",
+      img: "/images/products/code_atelier.png",
+      discription: "技術ブログサイト",
+    },
     { name: "ally", img: "/images/woman.webp" },
     { name: "Spotify Playlist", img: "/images/woman.webp" },
   ];
@@ -30,29 +34,32 @@ const Product: NextPage = () => {
           PRODUCTS
         </h1>
         <div className="grid grid-cols-2 gap-20">
-          {products.map((product) => (
+          {products.map((product, index) => (
             <div
-              key={product.name}
-              className="aspect-w-16 aspect-h-9 overflow-hidden opacity-70 duration-700 hover:opacity-30"
+              key={index}
+              className="aspect-w-16 aspect-h-9 overflow-hidden opacity-80 duration-700"
             >
               <Image
                 src={product.img}
                 alt={product.name}
-                layout="fill"
-                objectFit="cover"
+                width={500}
+                height={500}
                 className={cn(
-                  "duration-700 ease-in-out group-hover:opacity-40",
+                  "bg-gray-200 object-contain duration-700 ease-in-out group-hover:opacity-40",
                   isLoading
                     ? "scale-100 blur-2xl grayscale"
                     : "scale-100 blur-0 grayscale-0"
                 )}
                 onLoadingComplete={() => setLoading(false)}
               />
-              <h1
-                className={`${roboto.className} fadein-bottom flex items-end p-6 text-2xl hover:opacity-100`}
-              >
-                {product.name}
-              </h1>
+              <div className="fadein-bottom z-10 flex flex-col items-center justify-center text-black backdrop-blur-sm">
+                <h1
+                  className={`${roboto.className} p-2 text-2xl text-opacity-100`}
+                >
+                  {product.name}
+                </h1>
+                <p>{product.discription}</p>
+              </div>
             </div>
           ))}
         </div>
