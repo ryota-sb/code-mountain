4138fa32f716f3e9ca150bd8448f859b9324bcdf
ryota-sb <ryota.sb.1206@gmail.com>
2023-06-07 22:08:55 +0900

    データをjsonファイルから取得するように変更

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,25 +1,18 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import { useState } from "react";
 
 import Layout from "components/Layout";
 
+import data from "data/data.json";
+
 import { Montserrat } from "next/font/google";
 const roboto = Montserrat({ subsets: ["latin"], weight: "700" });
 
-const Product: NextPage = () => {
+const Products: NextPage = () => {
   const [isLoading, setLoading] = useState(true);
-
-  const products = [
-    {
-      name: "Code Atelier",
-      img: "/images/products/code_atelier.png",
-      discription: "技術ブログサイト",
-    },
-    { name: "ally", img: "/images/woman.webp" },
-    { name: "Spotify Playlist", img: "/images/woman.webp" },
-  ];
 
   const cn = (...classes: string[]) => {
     return classes.filter(Boolean).join(" ");
@@ -35,32 +28,33 @@ const Product: NextPage = () => {
             PRODUCTS
           </h1>
           <div className="grid grid-cols-1 gap-8 md:gap-16 lg:grid-cols-2 lg:gap-20">
-            {products.map((product, index) => (
-              <div
-                key={index}
-                className={cn(
-                  "aspect-w-16 aspect-h-9 overflow-hidden bg-gray-200 opacity-80 duration-700 ease-in-out group-hover:opacity-40",
-                  isLoading
-                    ? "scale-100 blur-2xl grayscale"
-                    : "scale-100 blur-0 grayscale-0"
-                )}
-              >
-                <Image
-                  src={product.img}
-                  alt={product.name}
-                  fill
-                  className="object-cover"
-                  onLoadingComplete={() => setLoading(false)}
-                />
-                <div className="fadein-bottom z-10 flex flex-col items-center justify-center text-black backdrop-blur-sm">
-                  <h1
-                    className={`${roboto.className} p-2 text-2xl text-opacity-100`}
-                  >
-                    {product.name}
-                  </h1>
-                  <p>{product.discription}</p>
+            {data.map((product, index) => (
+              <Link key={index} href={`/products/${product.id}`}>
+                <div
+                  className={cn(
+                    "aspect-w-16 aspect-h-9 overflow-hidden bg-gray-200 opacity-80 duration-700 ease-in-out group-hover:opacity-40",
+                    isLoading
+                      ? "scale-100 blur-2xl grayscale"
+                      : "scale-100 blur-0 grayscale-0"
+                  )}
+                >
+                  <Image
+                    src={product.image}
+                    alt={product.title}
+                    fill
+                    className="object-cover"
+                    onLoadingComplete={() => setLoading(false)}
+                  />
+                  <div className="fadein-bottom z-10 flex flex-col items-center justify-center text-black backdrop-blur-sm">
+                    <h1
+                      className={`${roboto.className} p-2 text-2xl text-opacity-100`}
+                    >
+                      {product.title}
+                    </h1>
+                    <p>{product.subTitle}</p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </section>
@@ -69,4 +63,4 @@ const Product: NextPage = () => {
   );
 };
 
-export default Product;
+export default Products;
